e779a72ff9c67e628e784798d768f65e1ce3b441
Francis Huang <huangf@missouri.edu>
2025-04-18 09:42:39 -0500

    Update Imputation Syntax 2025.R

--- LSA_imputation_2025/Imputation Syntax 2025.R
+++ LSA_imputation_2025/Imputation Syntax 2025.R
@@ -2,6 +2,7 @@
 ## Huang, F., & Keller, B. (2025). Working with missing data in large-scale assessments. Large-scale Assessments in 
 ## Education. doi: 10.1186/s40536-025-00248-9
 ## Load the required packages
+## Updated 2025.04.18 / using updated version of rblimp which can use factor variables
 
 library(MLMusingR) # for mixPV and summary for pooling
 library(WeMix) # for mix
@@ -39,13 +40,15 @@ m <- 20 #number of datasets to impute
 ns <- nrow(wmiss) #count how many observations there are
 tall$.pv <- as.numeric(gsub("[^0-9]", "", tall$name)) #extract the numeric value 
 nopv <- length(table(as.character(tall$.pv))) #the number of plausible values
-tall_numeric <- mutate(tall,
-  across(everything(), as.numeric) #blimp will only work with numeric data
-) 
+
+# NOTE: there is no need to do this step anymore with the updated version of rblimp
+# tall_numeric <- mutate(tall,
+#  across(everything(), as.numeric) #blimp will only work with numeric data
+# )  
 
 ## Impute the data
 mymodel <- rblimp(
-  data = tall_numeric,
+  data = tall, #this is using a different dataset as a result of the updated rblimp
   nominal = 'gender immig2 lackstaff',
   # ordinal = '',
   clusterid = 'cntschid',
@@ -69,13 +72,13 @@ impdat <- lapply(mymodel, as.mitml) |>
 
 impdat$.implist <- rep(1:(m * nopv), each = ns)
 
+## NO NEED FOR THIS STEP ANYMORE with the updated rblimp
 ## Return factor attributes to the data
-table(impdat$lackstaff)
-attributes(impdat$lackstaff) <- attributes(comb$lackstaff)
-table(impdat$lackstaff)
-
-attributes(impdat$gender) <- attributes(comb$gender)
-attributes(impdat$immig2) <- attributes(comb$immig2)
+# table(impdat$lackstaff)
+# attributes(impdat$lackstaff) <- attributes(comb$lackstaff)
+# table(impdat$lackstaff)
+# attributes(impdat$gender) <- attributes(comb$gender)
+# attributes(impdat$immig2) <- attributes(comb$immig2)
 
 ## Create a list for analysis
 alldat <- split(impdat, impdat$.implist)
